b7188003e6c312f177bd8008805ed12afb718e38
Ahmed Hassanein <eng.a7mad.gamal@gmail.com>
2025-07-22 22:11:01 +0200

    feat: Update ESLint configuration to include import plugin and add TypeScript type checking in pre-commit hook

--- .husky/pre-commit.ps1
+++ .husky/pre-commit.ps1
@@ -4,6 +4,11 @@ Write-Host "Running lint-staged..."
 npx lint-staged
 Write-Host "Running npm run lint..."
 npm run lint
+Write-Host "Running TypeScript type check..."
+if (!(npm run tsc)) {
+  Write-Host "TypeScript type check failed. Commit aborted."
+  exit 1
+}
 Write-Host "Running tests..."
 npm test
 Write-Host "Running prettier check..."
